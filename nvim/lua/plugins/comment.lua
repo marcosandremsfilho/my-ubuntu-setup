f5d8b63bae9f0fdeb32b3eeac13f0609380644af
marcosandremsfilho <marcos32andre@gmail.com>
2025-03-12 17:49:38 -0300

    feat: nvim: add the option to comment a code line

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -3,6 +3,12 @@ return {
   lazy = false,
   config = function()
     require('Comment').setup({
+       toggler = {
+        ---Line-comment toggle keymap
+        line = 'cll',
+        ---Block-comment toggle keymap
+        block = 'cbb',
+    },
       opleader = {
         ---Line-comment keymap
         line = 'cl',
