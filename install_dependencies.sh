6955cf8df7b47c101e05d1035f8a2c8909d933b4
marcosandremsfilho <marcos32andre@gmail.com>
2025-03-13 18:46:35 -0300

    fix: install dependencies doing a sudo command

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt install npm
-sudo apt install ripgrep # This dependencie is used to enable the live grep from telescope
+apt install npm
+apt install ripgrep # This dependencie is used to enable the live grep from telescope
