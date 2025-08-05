5c70cced503f0c49209f90a59d1c40b5980119af
marcos <marcos32andre@gmail.com>
2025-08-05 16:14:17 -0300

    fix: install_dependecies: tmux catpuccin theme bad installation

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,6 +3,11 @@
 apt install npm
 apt install ripgrep # This dependencie is used to enable the live grep from telescope
 
-cd ~/.config/
-git clone https://github.com/catppuccin/tmux.git
-cd -
+ORIG_USER=$SUDO_USER
+ORIG_HOME=$(eval echo "~$ORIG_USER")
+
+mkdir -p $ORIG_HOME/.config/tmux/plugins/catppuccin/tmux
+
+git clone https://github.com/catppuccin/tmux.git $ORIG_HOME/.config/tmux/plugins/catppuccin/tmux
+
+chown -R $ORIG_USER:$ORIG_USER $ORIG_HOME/.config/tmux
